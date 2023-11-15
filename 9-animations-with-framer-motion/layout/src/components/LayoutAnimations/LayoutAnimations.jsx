cae176d3d3645f0f95ecc93e1005d8dfaaa1760f
bora <nhatbaoxxd@gmail.com>
2023-11-16 01:05:00 +0700

    04. Layout Animations (final)

--- 9-animations-with-framer-motion/layout/src/components/LayoutAnimations/LayoutAnimations.jsx
+++ 9-animations-with-framer-motion/layout/src/components/LayoutAnimations/LayoutAnimations.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
+import { motion } from 'framer-motion'
+
+const SPRING = {
+  type: 'spring',
+  stiffness: 400,
+  damping: 40,
+}
 
 export default function LayoutAnimations() {
   const [isMaximized, setIsMaximized] = useState(false)
 
   return (
     <>
-      <div className={`wrapper ${isMaximized ? 'maximized' : ''}`} />
+      <motion.div layout={true} transition={SPRING} className={`wrapper ${isMaximized ? 'maximized' : ''}`}>
+        <motion.p layout="position" transition={SPRING}>
+          Hello world
+        </motion.p>
+      </motion.div>
 
       <button onClick={() => setIsMaximized(!isMaximized)}>Toggle</button>
     </>
