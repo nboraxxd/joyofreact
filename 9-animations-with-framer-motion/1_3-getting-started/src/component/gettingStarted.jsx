25412151880b0df80332a78539c7ae98002a8cfe
bora <nhatbaoxxd@gmail.com>
2023-11-15 16:04:13 +0700

    01. Getting Started (final)

--- 9-animations-with-framer-motion/1_3-getting-started/src/component/gettingStarted.jsx
+++ 9-animations-with-framer-motion/1_3-getting-started/src/component/gettingStarted.jsx
@@ -2,11 +2,20 @@ import { useState } from 'react'
 import { motion } from 'framer-motion'
 
 function Toggle() {
-  const [isEnabled, setIsEnabled] = useState(false)
+  const [isEnabled, setIsEnabled] = useState(true)
 
   return (
     <>
-      <div className="yellow ball" />
+      <motion.div
+        initial={false}
+        animate={{ y: isEnabled ? 60 : 0 }}
+        className="yellow ball"
+        transition={{
+          type: 'spring', // default is spring value
+          stiffness: 200, // độ cứng
+          // damping: 0, // giảm độ nảy, damping càng lớn, độ nảy càng thấp
+        }}
+      />
       <button onClick={() => setIsEnabled(!isEnabled)}>Toggle</button>
     </>
   )
