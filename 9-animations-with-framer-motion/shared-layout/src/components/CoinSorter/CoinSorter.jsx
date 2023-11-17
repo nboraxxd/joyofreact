a2e7b4508f8513cfd2d8f1fd607bf13210fed070
bora <nhatbaoxxd@gmail.com>
2023-11-17 17:15:09 +0700

    08. Shared layout exercise 2 (final)

--- 9-animations-with-framer-motion/shared-layout/src/components/CoinSorter/CoinSorter.jsx
+++ 9-animations-with-framer-motion/shared-layout/src/components/CoinSorter/CoinSorter.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import range from 'lodash.range'
-import { motion } from 'framer-motion'
+import React, { useId } from 'react'
+import range from 'lodash/range'
+import { LayoutGroup, motion } from 'framer-motion'
 
 import styles from './CoinSorter.module.css'
 
@@ -8,21 +8,36 @@ const NUM_OF_BOXES = 4
 
 export default function CoinSorter({ numOfCoins }) {
   const [selectedBox, setSelectedBox] = React.useState(0)
+  const id = useId()
 
   return (
-    <div className={styles.wrapper}>
-      {range(NUM_OF_BOXES).map((boxIndex) => (
-        <button
-          key={boxIndex}
-          className={`${styles.box} ${selectedBox === boxIndex ? styles.selected : ''}`}
-          onClick={() => setSelectedBox(boxIndex)}
-        >
-          {selectedBox === boxIndex &&
-            range(numOfCoins).map((coinIndex) => {
-              return <div key={coinIndex} className={styles.coin} />
-            })}
-        </button>
-      ))}
-    </div>
+    <LayoutGroup>
+      <div className={styles.wrapper}>
+        {range(NUM_OF_BOXES).map((boxIndex) => (
+          <button
+            key={boxIndex}
+            className={`${styles.box} ${selectedBox === boxIndex ? styles.selected : ''}`}
+            onClick={() => setSelectedBox(boxIndex)}
+          >
+            {selectedBox === boxIndex &&
+              range(numOfCoins).map((coinIndex) => {
+                const layoutId = `${id}-${coinIndex}`
+                return (
+                  <motion.div
+                    layoutId={layoutId}
+                    key={layoutId}
+                    className={styles.coin}
+                    transition={{
+                      type: 'spring',
+                      stiffness: 400,
+                      damping: 40 + coinIndex * 5,
+                    }}
+                  />
+                )
+              })}
+          </button>
+        ))}
+      </div>
+    </LayoutGroup>
   )
 }
